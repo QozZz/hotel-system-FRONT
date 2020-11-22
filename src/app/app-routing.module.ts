3d6614e3da05f3cce30239520560aac9befd1b7a
Dmytro Zhelieznyi <dmytro_zhelieznyi@epam.com>
2020-11-22 02:17:39 +0200

    created home-page, hotel-list (module/component/routing-module), set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {NotFoundPageComponent} from './not-found-page/not-found-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../app/home-page/home-page.module').then(m => m.HomePageModule),
+  },
+  {
+    path: 'hotels',
+    loadChildren: () => import('../app/hotel-list/hotel-list.module').then(m => m.HotelListModule),
+  },
+  {
+    path: '**', component: NotFoundPageComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
