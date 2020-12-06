90d84677b69838d219abb0c20f80b9b0aa5bdba3
Dmytro Zhelieznyi <dmytro_zhelieznyi@epam.com>
2020-12-06 20:09:48 +0200

    Prepared filtering block

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -9,23 +9,45 @@ import {Hotel} from '../shared/models/Hotel';
 })
 export class HotelListComponent implements OnInit {
 
+  hotelsCache: Hotel[];
   hotels: Hotel[];
+
+  starsFilter: number[];
 
   constructor(
     private hotelService: HotelService
-  ) {}
-
-  ngOnInit(): void {
-    this.getHotelList();
+  ) {
   }
 
-  getHotelList() {
-    this.hotelService.getAll()
-      .subscribe(hotels => this.hotels = hotels, error => console.log(error));
+  ngOnInit(): void {
+    this.hotelService.getAll().subscribe(hotels => {
+      this.hotelsCache = hotels;
+      this.hotels = hotels;
+    }, error => console.log(error));
+    this.starsFilter = [];
   }
 
   getCounter(num: number): Array<number> {
     return new Array(num);
+  }
+
+  addStarsFilter(event: any) {
+    const value = Number(event.target.value);
+    if (!this.starsFilter.includes(value)) {
+      this.starsFilter.push(value);
+    } else {
+      this.starsFilter.splice(this.starsFilter.indexOf(value), 1);
+    }
+    this.filter();
+  }
+
+  filter() {
+    if (this.starsFilter.length === 0) {
+      this.hotels = this.hotelsCache;
+    } else {
+      this.hotels = this.hotelsCache
+        .filter(value => this.starsFilter.includes(value.stars));
+    }
   }
 
 }
